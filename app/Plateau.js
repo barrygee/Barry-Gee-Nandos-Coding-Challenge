27d7ee65c6486e120b837f8e7f972d3c55b9f435
Barry <barrymarkgee@gmail.com>
2021-02-28 17:51:53 +0000

    Add Plateau class

---
+++ app/Plateau.js
@@ -0,0 +1,46 @@
+class Plateau {
+    constructor(upperRightCoordinates) {
+
+        if(!upperRightCoordinates) { 
+            throw new Error('Invalid coordinates data provided'); 
+        } else {
+            this.upperRightCoordinates = upperRightCoordinates;
+        } 
+        
+        this.cardinalCompassPoints = ['N', 'E', 'S', 'W'];
+    }
+
+    set upperRightCoordinates(upperRightCoordinates) {
+        // destructure the upperRightX, upperRightY values from the upperRightCoordinates array 
+        const [ upperRightX, upperRightY ] = upperRightCoordinates;
+
+        this.upperRightX = upperRightX;
+        this.upperRightY = upperRightY;
+    }
+
+    get upperRightX() {
+        return this._upperRightX;
+    }
+
+    set upperRightX(upperRightX) {
+        this._upperRightX = upperRightX;
+    }
+
+    get upperRightY() {
+        return this._upperRightY;
+    }
+
+    set upperRightY(upperRightY) {
+        this._upperRightY = upperRightY;
+    }
+
+    get cardinalCompassPoints() {
+        return this._cardinalCompassPoints;
+    }
+
+    set cardinalCompassPoints(cardinalCompassPoints) {
+        this._cardinalCompassPoints = cardinalCompassPoints;
+    }
+}
+
+module.exports = Plateau;
